724e00bda555e72e3aa951b75c9de31f359c940e
Eduardo Ulises Hernández Medina <110645665+InaliQ@users.noreply.github.com>
2024-11-09 23:00:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ app.include_router(sales.router)
 
 if __name__ == "__main__":
     # Obtenemos el puerto desde la variable de entorno o usamos el 8000 por defecto
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
